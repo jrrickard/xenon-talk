02eb1cfbb72e21c623da8f90fba5f23b98dbb393
Jeremy Rickard <jrickard@vmware.com>
2017-08-06 17:17:47 -0600

    Adding a Patch Operation

--- src/main/java/io/github/jrrickard/services/MeetupService.java
+++ src/main/java/io/github/jrrickard/services/MeetupService.java
@@ -1,5 +1,9 @@
 package io.github.jrrickard.services;
 
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ReflectionUtils;
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.ServiceDocumentDescription;
 import com.vmware.xenon.common.StatefulService;
 import io.github.jrrickard.podo.Meetup;
 
@@ -12,4 +16,31 @@ public class MeetupService extends StatefulService {
         super.toggleOption(ServiceOption.REPLICATION, true);
     }
 
+    @Override
+    public void handlePatch(final Operation patch) {
+        final Meetup currentState = getState(patch);
+        final Meetup patchState = patch.getBody(Meetup.class);
+
+        try {
+            merge(getDocumentTemplate().documentDescription, currentState, patchState);
+        } catch (final Exception e) {
+            patch.fail(e, "Unable to patch");
+        }
+        patch.complete();
+    }
+    private void merge(final ServiceDocumentDescription desc, final Meetup source, final Meetup patch) {
+        final Class<? extends ServiceDocument> clazz = source.getClass();
+        if (!patch.getClass().equals(clazz)) {
+            throw new IllegalArgumentException("Source object and patch object types mismatch");
+        }
+        for (final ServiceDocumentDescription.PropertyDescription prop : desc.propertyDescriptions.values()) {
+            if (prop.usageOptions != null && prop.usageOptions.contains(ServiceDocumentDescription.PropertyUsageOption.AUTO_MERGE_IF_NOT_NULL)) {
+                Object o = ReflectionUtils.getPropertyValue(prop, patch);
+                if (o != null && !o.equals(ReflectionUtils.getPropertyValue(prop, source))) {
+                    ReflectionUtils.setPropertyValue(prop, source, o);
+                  ;
+                }
+            }
+        }
+    }
 }
